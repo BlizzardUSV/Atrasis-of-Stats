97b682ec0a61e4257d05d7e5a690611552321a3b
BlizzardUSV <93410076+BlizzardUSV@users.noreply.github.com>
2021-11-28 21:36:28 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -1,4 +1,10 @@
 <?php
+
+    $server = "sql101.epizy.com";
+    $username = "epiz_30465896";
+    $password = "dDOrLwbIAuhgWH5"
+    $dbname = "epiz_30465896_database_tags_original"
+
 if(isset($_POST['envoi'])){ // si formulaire soumis
 echo $_POST['tag_rentré'];
 }
